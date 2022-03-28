42c25e570f3e6e02dc567aed98062d32f80d4541
Austin Cosby <akc369@msstate.edu>
2022-03-27 23:50:29 -0500

    finalized jam game

--- Assets/Scripts/MusicHandler.cs
+++ Assets/Scripts/MusicHandler.cs
@@ -4,15 +4,9 @@ using UnityEngine;
 
 public class MusicHandler : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    void Awake()
     {
         
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        DontDestroyOnLoad(this.gameObject);
     }
 }
